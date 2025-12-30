93ebfb1d42b1dd69b7806039a50554d3bbebf0d0
Ojas Ankit <ojasankit@gmail.com>
2025-12-30 20:45:46 +0530

    added ui for rock paper scissors

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-let computerChoices = ['rock', 'paper', 'scissors']
-let humanScore = 0, computerScore = 0
+const body = document.body
+const span = document.createElement("span")
+const buttonRock = document.createElement("button")
+const buttonPaper = document.createElement("button")
+const buttonScissors = document.createElement("button")
+
+span.textContent = "Select your choice: "
+buttonRock.textContent = "Rock"
+buttonPaper.textContent = "Paper"
+buttonScissors.textContent = "Scissors" 
+
+body.append(span)
+body.append(buttonRock)
+body.append(buttonPaper)
+body.append(buttonScissors)
+
+let computerChoices = ["rock", "paper", "scissors"]
+let humanScore = 0, computerScore = 0, roundCount = 1;
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    return computerChoices[computerChoice];
+    let computerChoice = Math.floor(Math.random() * 3)
+    return computerChoices[computerChoice]
 }
 
 function getHumanChoice() {
@@ -11,34 +27,68 @@ function getHumanChoice() {
     return humanChoice.toLowerCase()
 }
 
-function playRound() {
-    const humanChoice = getHumanChoice()
-    const computerChoice = getComputerChoice()
-    let resultString = ""
+function checkGameEnd() {
+    if(computerScore == 5 || humanScore == 5) {
+        buttonRock.disabled = true;
+        buttonPaper.disabled = true;
+        buttonScissors.disabled = true;
 
+        const restartButton = document.createElement("button");
+        restartButton.textContent = "Restart Game?"
+        restartButton.addEventListener("click", (e) => {
+            window.location.reload("true")
+        })
+
+        alert(`Game finished! ${computerScore > humanScore ? "Computer wins!" : "You win!"}`)
+        body.append(restartButton)
+    } else {
+        return
+    }
+
+}
+
+function playRound(humanChoice) {
+    const choiceDiv = document.createElement("div")
+    const resultDiv = document.createElement("div")
+    const computerChoice = getComputerChoice()
+
+    choiceDiv.textContent = `You picked ${humanChoice} and the computer picked ${computerChoice}!`
+    
     if(humanChoice == computerChoice) {
-        resultString = `Tie! Both players picked ${computerChoice}!`
+        resultDiv.textContent = `Tie! Both players picked ${computerChoice}!`
     } else if (
-        (humanChoice == 'rock' && computerChoice == 'scissors') ||
-        (humanChoice == 'scissors' && computerChoice == 'paper') ||
-        (humanChoice == 'paper' && computerChoice == 'rock')) {
-        resultString = `You win! ${humanChoice} beats ${computerChoice}!`
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "rock")) {
+        resultDiv.textContent = `You win! ${humanChoice} beats ${computerChoice}!`
         humanScore ++
     } else {
-        resultString = `Computer wins! ${computerChoice} beats ${humanChoice}!`
+        resultDiv.textContent = `Computer wins! ${computerChoice} beats ${humanChoice}!`
         computerScore ++
     }
-    console.log(resultString);
+
+    body.append(document.createElement("br"))
+    body.append(document.createTextNode(`Round ${roundCount}:`))
+    body.append(choiceDiv)
+    body.append(resultDiv)
+    body.append(document.createTextNode(`Current Score - Computer: ${computerScore} You: ${humanScore}`))
+    body.append(document.createElement("br"))
+    checkGameEnd();
+
+    roundCount ++;
 }
 
-console.log("Starting the game!");
-for(let i = 0; i < 5; i ++) {
-    console.log(`Round ${i + 1}:`);
-    playRound()
-}
+buttonRock.addEventListener("click", () => {
+    playRound("rock")
+    console.log("User picked rock")
+})
 
-if(computerScore > humanScore) {
-    console.log(`Computer wins! Computer: ${computerScore} | You: ${humanScore}`);
-} else {
-    console.log(`You win! Computer: ${computerScore} | You: ${humanScore}`);
-}
+buttonPaper.addEventListener("click", () => {
+    playRound("paper")
+    console.log("User picked paper")
+})
+
+buttonScissors.addEventListener("click", () => {
+    playRound("scissors")
+    console.log("User picked scissors")
+})
